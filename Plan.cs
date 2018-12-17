8f7c8e809650c5c768788ba5e45e2daab7e7e4cd
Héloïse Zhang <heloise.zhang@edu.ece.fr>
2018-12-17 15:19:49 +0100

    modif de l'algo de l'itinéraire

--- Plan.cs
+++ Plan.cs
@@ -6,43 +6,39 @@ namespace modele
 {
     public class Plan // plan logique
     {
-        // nom de la salle de départ et d'arrivée
-        string depart, arrivee;
-        // objet case qui correspond respectivement à la salle de départ et d'arrivée
-        CaseNoeud caseDepart, caseArrivee;
+        private int nbCol, nbLig;
         // grille contenant la liste des cases
         List<Case> grille;
 
         // constructeur
         public Plan()
         {
-            depart = null;
-            arrivee = null;
-            caseDepart = new CaseNoeud();
-            caseArrivee = new CaseNoeud();
+            nbCol = null;
+            nbLig = null;  
             grille = new List<Case>();
         }
         // dans le cas d'une recherche avec point de départ et d'arrivée
-        public Plan(string dep, string arr)
+        public Plan(int nbCol, int nbLig, List<Case> grille)
         {
-            depart = dep;
-            arrivee = arr;
-            caseDepart = new CaseNoeud();
-            caseArrivee = new CaseNoeud();
+            this.nbCol = nbCol;
+            this.nbLig = nbLig;  
+            this.grille = grille; 
         }
 
-        public List<ListNoeud> rechercheItiNoeud()
+        public List<ListNoeud> rechercheItiNoeud(CaseNoeud depart, CaseNoeud arrivee)
         {
+            // contient liste de tous les itinéraires possibles
             List<ListNoeud> listeList = new List<ListNoeud>();
-            List<CaseNoeud> listCaseNoeudDep = new List<CaseNoeud>();
-            List<CaseNoeud> listCaseNoeudAr = new List<CaseNoeud>();
-            setCaseDep(depart);
-            setCaseAr(arrivee);
-            listCaseNoeudDep = caseDepart.getVoisin();
-            listCaseNoeudAr = caseArrivee.getVoisin();
+            List<CaseNoeud> listCaseNoeudDep = depart.getVoisin();
+            List<CaseNoeud> listCaseNoeudAr = arrivee.getVoisin();
+ 
             // taille de la liste de noeud de la case départ, on suppose que dans tous les cas la valeur est toujours de 2, chaque couloir 2 noeuds
             int n = listCaseNoeudDep.Count;
+            // nombre d'éléments dans la liste des listes
             int m = 0;
+
+            // création de liste selon le nombre de noeuds de départ
+            // ajout de ces listes dans la liste des listes
             for (int i = 0; i < n; i++)
             {
                 ListNoeud liste = new ListNoeud();
@@ -50,17 +46,20 @@ namespace modele
                 listeList.Add(liste);
                 m = listeList.Count;
             }
-
-            while (m != n)
+            // boolean vérifiant sur les derniers éléments sont ceux de la destination
+            Boolean check;
+            // acquisition des différents itinéraires de noeuds possibles
+            while (check == false)
             {
-                n = m;
+                n = listeList.Count;
                 // liste de transition
                 List<ListNoeud> listeTran = new List<ListNoeud>();
                 for (int i = 0; i < n; i++)
                 {
+                    // liste qui contient les caseNoeuds de 1 itinéraire
                     ListNoeud liste = listeList[i];
                     //ListNoeud l = new ListNoeud();
-
+                    // t: index du dernier élément de la liste en cours
                     int t = liste.getListe().Count;
                     CaseNoeud c = liste.getListe()[t - 1];
                     if ((compareCase(c, listCaseNoeudAr[0]) == false) && (compareCase(c, listCaseNoeudAr[1]) == false))
@@ -74,7 +73,7 @@ namespace modele
                 }
 
                 listeList = listeTran;
-                m = listeList.Count;
+                // fonction qui retourne boolean après vérification
             }
             // méthode qui verifie si tous les ititnéraires possibles arrivent à destination
             return listeList;
@@ -97,37 +96,6 @@ namespace modele
             }
             return false;
         }
-
-        // méthode pour voir si le point de départ est dans le même couloir que le point d'arrivée
-        public CaseNoeud getCaseDep()
-        {
-            return caseDepart;
-        }
-
-        public CaseNoeud getCaseArr()
-        {
-            return caseArrivee;
-        }
-
-        // setter pour la case correpondant au départ, de la même manière que pour l'arrivee
-        public void setCaseDep(string dep)
-        {
-            //     foreach (Case c in grille)
-            //     {
-            //         if (c.)
-            //     }
-        }
-
-        public void setCaseAr(string arr)
-        {
-            //     foreach (Case c in grille)
-            //     {
-            //         if (c.)
-            //     }
-        }
-
-
-
 
     }
 
